f6b3e687edfa40894b8744c034f9e2208e47baf2
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-10 22:07:31 -0500

    incorporating useReducer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,39 +1,19 @@
 import "./App.css"
-import { useState, useEffect } from "react"
+import { useState, useReducer } from "react"
 
 function App() {
-  /**
-   * const what = useState('currentState');
-   * console.log(what);
-   * [currentState, setFunction]
-   * */
-
-  const [emotion, setEmotion] = useState("Happy Happy")
-
-  useEffect(
-    () => {
-      console.log(`I'm ${emotion} right now`)
-    },
-    [emotion] //This is the dependency array, it's only called when this changes. Empty just once
-  )
-
-  const [secondary, setSecondary] = useState("tired")
-
-  useEffect(
-    () => {
-      console.log(`I'm ${emotion} and very ${secondary} right now`)
-    },
-    [secondary, emotion] //Funciona como un .start, solo se activa cuando algo cambio
-  )
+  // const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useReducer((checked) => !checked, false)
 
   return (
     <div className="App">
-      <h1>My current Emotion is: {emotion} </h1>
-      <button onClick={() => setEmotion("Sed")}>I'm sad</button>
-      <button onClick={() => setEmotion("Excited")}>I'm Excited</button>
-      <h1>My secondary Emotion is: {secondary} </h1>
-      <button onClick={() => setSecondary("Grateful")}>I'm Grateful</button>
-      <button onClick={() => setSecondary("Awake")}>I'm Awake</button>
+      <input
+        type="checkbox"
+        value={checked}
+        // onChange={() => setChecked((checked) => !checked)}
+        onChange={setChecked}
+      />
+      <label>{checked ? "Checked" : "not checked"} </label>
     </div>
   )
 }
