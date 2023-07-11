53e692539f6a5df6c27f8ce05acb1ca069a2993a
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-10 22:01:30 -0500

    Understanding the dependency array

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,20 +10,30 @@ function App() {
 
   const [emotion, setEmotion] = useState("Happy Happy")
 
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`)
-  }, [emotion] //This is the dependency array, it's only called when this changes. Empty just once
-  );
+  useEffect(
+    () => {
+      console.log(`I'm ${emotion} right now`)
+    },
+    [emotion] //This is the dependency array, it's only called when this changes. Empty just once
+  )
+
+  const [secondary, setSecondary] = useState("tired")
+
+  useEffect(
+    () => {
+      console.log(`I'm ${emotion} and very ${secondary} right now`)
+    },
+    [secondary, emotion] //Funciona como un .start, solo se activa cuando algo cambio
+  )
 
   return (
     <div className="App">
       <h1>My current Emotion is: {emotion} </h1>
-      <button onClick={() => setEmotion("Sed")}>
-        I'm sad
-      </button>
-      <button onClick={() => setEmotion("Excited")}>
-        I'm Excited
-      </button>
+      <button onClick={() => setEmotion("Sed")}>I'm sad</button>
+      <button onClick={() => setEmotion("Excited")}>I'm Excited</button>
+      <h1>My secondary Emotion is: {secondary} </h1>
+      <button onClick={() => setSecondary("Grateful")}>I'm Grateful</button>
+      <button onClick={() => setSecondary("Awake")}>I'm Awake</button>
     </div>
   )
 }
