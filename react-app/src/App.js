4984afb3b0367590e89191a773cd3bb3e119bfea
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-12 14:23:52 -0500

    Fetching data with GraphQL

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,41 +1,63 @@
 import "./App.css"
 import { useState, useEffect } from "react"
 
-function GithubUser({name, location, company, avatar}){
-  return(
+// GraphQL Playground = https://snowtooth.moonhighway.com/
+
+const query = `
+query {
+  allLifts {
+    name,
+    elevationGain,
+    status
+  }
+}`
+
+const opts = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ query }),
+}
+
+function Lift({ name, elevationGain, status }) {
+  return (
     <div>
       <h1>{name}</h1>
-      <p>{location}</p>
-      <p>{company}</p>
-      <img src={avatar} height={150} alt="Sebastian logo"/>
+      <p>
+        {elevationGain} {status}
+      </p>
     </div>
   )
 }
 
 function App() {
   const [data, setData] = useState(null)
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    setLoading(true);
-    fetch("https://api.github.com/users/juasmartinezbelS4N")
+    setLoading(true)
+    fetch("https://snowtooth.moonhighway.com/", opts)
       .then((response) => response.json())
       .then(setData)
-      .then(()=>setLoading(false))
+      .then(() => setLoading(false))
       .catch(setError) //(error) => {setError(error)}
   }, [])
 
-  if(loading) return <h1>Loading</h1>
-  if(error) return <pre>{JSON.stringify(error)}</pre>
-  if(!data) return null
-
-  if (data) {
-    return <GithubUser name={data.name} location={data.location}
-      company={data.company}
-      avatar={data.avatar_url} />
-  }
-  return <div className="App">Data</div>
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <pre>{JSON.stringify(error)}</pre>
+  if (!data) return null
+  // FOR SOME WEIRD ASS REASON, THERE IS A DATA.DATA in GRAPHQL
+  return (
+    <div>
+      {data.data.allLifts.map((lift) => (
+        <Lift
+          name={lift.name}
+          elevationGain={lift.elevationGain}
+          status={lift.status}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default App
