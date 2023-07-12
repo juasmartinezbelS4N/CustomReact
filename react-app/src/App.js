e6afb75542ada6d718f52f47f91fb0d786ac8547
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-12 10:32:20 -0500

    Working with uncontrolled components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,28 @@
 import "./App.css"
-import { useState, useReducer } from "react"
+import { useRef } from "react"
+// useRef alcanza un elemento impuesto y usa su valor
 
 function App() {
-  // const [checked, setChecked] = useState(false)
-  const [checked, setChecked] = useReducer((checked) => !checked, false)
+  const txtTitle = useRef();
+  const hexColor = useRef();
 
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+    txtTitle.current.value = '';
+    hexColor.current.value = '';
+  }
   return (
     <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        // onChange={() => setChecked((checked) => !checked)}
-        onChange={setChecked}
-      />
-      <label>{checked ? "Checked" : "not checked"} </label>
+      <form onSubmit={submit}>
+        <input type="text"
+          ref={txtTitle}
+          placeholder="Color title..." />
+        <input ref={hexColor} type="color" />
+        <button>ADD</button>
+      </form>
     </div>
   )
 }
