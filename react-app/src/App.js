a669b29333f8404a5c8dd6d9f4ed5407ce775c2a
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-12 18:57:27 -0500

    Working with render props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,62 +1,36 @@
 import "./App.css"
 import { useState, useEffect } from "react"
 
-// GraphQL Playground = https://snowtooth.moonhighway.com/
-
-const query = `
-query {
-  allLifts {
-    name,
-    elevationGain,
-    status
-  }
-}`
-
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query }),
-}
-
-function Lift({ name, elevationGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
-    </div>
-  )
-}
-
 function App() {
-  const [data, setData] = useState(null)
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const tahoe_peaks = [
+    { name: "Freel", elevation: 10891 },
+    { name: "Monument", elevation: 10067 },
+    { name: "Pyramid", elevation: 9983 },
+    { name: "Tallac", elevation: 9735 },
+  ]
 
-  useEffect(() => {
-    setLoading(true)
-    fetch("https://snowtooth.moonhighway.com/", opts)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError) //(error) => {setError(error)}
-  }, [])
-
-  if (loading) return <h1>Loading...</h1>
-  if (error) return <pre>{JSON.stringify(error)}</pre>
-  if (!data) return null
-  // FOR SOME WEIRD ASS REASON, THERE IS A DATA.DATA in GRAPHQL
+  function List({ data, renderItem, renderEmpty }) {
+    return !data.length ? (
+      renderEmpty
+    ) : (
+      <ul>
+        {data.map((item) => {
+          return <li key={item.name}>{renderItem(item)}</li>
+        })}
+      </ul>
+    )
+  }
+  // <React.Fragment> -> <>
   return (
-    <div>
-      {data.data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          elevationGain={lift.elevationGain}
-          status={lift.status}
-        />
-      ))}
-    </div>
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation}
+        </>
+      )}
+    />
   )
 }
 
