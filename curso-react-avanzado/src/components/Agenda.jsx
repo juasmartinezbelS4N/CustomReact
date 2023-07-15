31b2593a3e64e1ae6469e901f51cab369660d823
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-14 22:26:16 -0500

    Portals en React

--- curso-react-avanzado/src/components/Agenda.jsx
+++ curso-react-avanzado/src/components/Agenda.jsx
@@ -1,22 +1,37 @@
-import React, { Component } from 'react'
-import ReservarHora from './ReservarHora'
+import React, { Component } from "react"
+import ReservarHora from "./ReservarHora"
+import Modal from "./Modal"
+import Confirmacion from "./Confirmacion"
 
 export default class Agenda extends Component {
-  confirm = () =>{
-    alert("OK")
+  constructor(props) {
+    super(props)
+    this.state = { modalVisible: false }
+  }
+
+  modalClickHandler = (e) => {
+    this.setState({ modalVisible: false })
+  }
+
+  confirm = () => {
+    this.setState({ modalVisible: true })
   }
 
   render() {
     return (
       <div>
         <h1 className="display-4">Doctor Nicolás</h1>
-        <div class="list-group">
+        <div className="list-group">
           <ReservarHora onClick={this.confirm} />
           <ReservarHora onClick={this.confirm} />
           <ReservarHora onClick={this.confirm} />
           <ReservarHora onClick={this.confirm} />
           <ReservarHora onClick={this.confirm} />
           <ReservarHora onClick={this.confirm} />
+        </div>
+
+        <div onClick={this.modalClickHandler}>
+          <Modal>{this.state.modalVisible && <Confirmacion />}</Modal>
         </div>
       </div>
     )
