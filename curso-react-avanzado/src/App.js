0702d72d19502278b0f7b5f795522c82b972d214
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-14 20:40:51 -0500

    Preparing for next class

--- curso-react-avanzado/src/App.js
+++ curso-react-avanzado/src/App.js
@@ -1,27 +1,10 @@
 import "./App.css";
-import ErrorBoundary from "./components/ErrorBoundary";
-import MiComponente from "./components/MiComponente";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="display-4">Tienda Online</h1>
-        <p className="lead"> Selecciona un producto </p>
-      </div>
-
-      <div className="container">
-        <div className="row">
-          {
-            /*Los Error Boundaries ayudan a mostrar información
-            si llega a haber un error*/
-          }
-          <ErrorBoundary>
-            <MiComponente />
-            <MiComponente title="Producto 2" />
-            <MiComponente title="Producto 3" />
-          </ErrorBoundary>
-        </div>
+        <h1 className="display-4">Doctor Nicolás</h1>
       </div>
     </div>
   );
--- curso-react-avanzado/src/components/MiError.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MiError() {
-    return (
-        <div className="text-danger" >
-            ERROR
-        </div>
-    )
-}
