e09caec320c1ed5fd40a11b1b99a551f897b55ea
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-16 18:02:33 -0500

    ---

--- curso-react-avanzado/src/App.js
+++ curso-react-avanzado/src/App.js
@@ -1,36 +1,11 @@
 import "./App.css"
 import Item from "./components/Item"
 
-const elementos = [
-  {
-    id: 1,
-    titulo: "Café",
-    definicion:
-      "Bebida que se hace por infusión con la semilla tostada y molida del cafeto.",
-  },
-  {
-    id: 1,
-    titulo: "Té",
-    definicion: "Infusión de las hojas del té.",
-  },
-  {
-    id: 1,
-    titulo: "Chocolate",
-    definicion:
-      "Bebida que se hace de chocolate desleído y cocido en agua o en leche.",
-  },
-]
-
 function App() {
-  const listado = elementos.map((miItem) => (
-    <Item titulo={miItem.titulo} definicion={miItem.definicion} />
-  ))
   return (
     <div className="App">
       <div className="container">
-        <dl>
-          {listado}
-        </dl>
+        Hola mundo
       </div>
     </div>
   )
