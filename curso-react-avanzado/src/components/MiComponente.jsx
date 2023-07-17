27f23fc76946e3554f7d943b5ddd38b1172801ec
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-16 19:53:27 -0500

    Capturar datos de un formulario en el estado ahora sí. Antes era en general

--- curso-react-avanzado/src/components/MiComponente.jsx
+++ curso-react-avanzado/src/components/MiComponente.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from "react"
 
 export default class MiComponente extends Component {
+  constructor(props) {
+    super(props) // Recordatorio que SUPER premite usar this.
+
+    this.state = {
+      item1: "",
+      item2: "",
+      item3: "",
+    }
+  }
 
   onSubmitHandler = (evento) => {
-    evento.preventDefault();
+    evento.preventDefault()
   }
 
   onNombreInputChange = (input) => {
-    console.log(input.target.value)
+    const {value} = input.target
+    this.setState({item1: value})
   }
 
   onApellidoInputChange = (input) => {
-    console.log(input.target.value)
+    const {value} = input.target
+    this.setState({item2: value})
   }
 
   onHobbyInputChange = (input) => {
-    console.log(input.target.value)
+    const {value} = input.target
+    this.setState({item3: value})
   }
 
   render() {
@@ -54,6 +66,9 @@ export default class MiComponente extends Component {
             </button>
           </div>
         </form>
+        <h1>Valor 1: {this.state.item1}</h1>
+        <h1>Valor 2: {this.state.item2}</h1>
+        <h1>Valor 3: {this.state.item3}</h1>
       </div>
     )
   }
