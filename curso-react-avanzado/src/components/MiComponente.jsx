f1e0253169e2dadf70b49a7904296f2d12f0f214
Sebastian Martinez <sebastian.martinez@lever.co>
2023-07-16 19:30:06 -0500

    Preparando un formulario en React

--- curso-react-avanzado/src/components/MiComponente.jsx
+++ curso-react-avanzado/src/components/MiComponente.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from "react"
 
 export default class MiComponente extends Component {
+
+  onSubmitHandler = (evento) => {
+    evento.preventDefault();
+  }
+
   render() {
     return (
       <div className="formularios col-md-5">
-        <form>
+        {/*<form> Por defecto hacer una etiqueta form, te redirige la página*/}
+        {/* Es mejor hacer un evento onClick para el botón submit Ó:*/}
+        <form onSubmit={this.onSubmitHandler}>
           <div className="form-group">
             <input
               type="text"
               className="form-control"
               name="item1"
-              placeholder=""
+              placeholder="Nombre"
+            />
+
+            <input
+              type="text"
+              name="item2"
+              className="form-control"
+              placeholder="Apellido"
+            />
+
+            <input
+              type="text"
+              name="item3"
+              className="form-control"
+              placeholder="Hobby"
             />
 
             <button className="btn btn-primary" type="submit">
